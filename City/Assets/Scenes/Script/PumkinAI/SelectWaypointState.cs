8d3be4da2dcb965ab85749cf009a5c8bfcdea0cb
RyanBan <45275840+RyanBan@users.noreply.github.com>
2020-12-06 04:23:57 -0500

    machine state added

---
+++ City/Assets/Scenes/Script/PumkinAI/SelectWaypointState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectWaypointState : StateMachineBehaviour {
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        PumkinAI pumkinAI = animator.gameObject.GetComponent<PumkinAI>();
+        pumkinAI.SetNextPoint();
+    }
+}
